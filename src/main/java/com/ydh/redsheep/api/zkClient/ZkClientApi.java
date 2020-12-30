7848c84dcd70702592c818f8fcbab87a4131c12f
yangdehong <405104787@qq.com>
2020-12-30 19:37:01 +0800

    zkClient

--- src/main/java/com/ydh/redsheep/api/zkClient/ZkClientApi.java
+++ src/main/java/com/ydh/redsheep/api/zkClient/ZkClientApi.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class ZkClientApi {
 
-    private static final String connectString = "172.16.165.222:2181";
+    private static final String connectString = "172.16.131.6:2181";
     private static int timeout = 30*1000;
 
     private static ZkClient zkClient;
@@ -24,14 +24,16 @@ public class ZkClientApi {
         // BytesPushThroughSerializer不是对象的时候，使用byte序列
         zkClient = new ZkClient(connectString, timeout, timeout, new SerializableSerializer());
         System.out.println(zkClient);
-        List<String> strings = zkClient.subscribeChildChanges("/node_zkClinet_1", new ZkClientChildListener());
+        // 监听子节点变化
+        List<String> strings = zkClient.subscribeChildChanges("/node_zkClient_1", new ZkClientChildListener());
         System.out.println(strings);
+        // 监听状态变化
         zkClient.subscribeStateChanges(new ZkClientStateListener());
-        zkClient.subscribeDataChanges("/node_zkClinet_1", new ZkClientDataListener());
+        // 监听节点数据变化
+        zkClient.subscribeDataChanges("/node_zkClient_1", new ZkClientDataListener());
 
-
-//        createNode();
-        updateData();
+        createNode();
+//        updateData();
 //        deleteNode();
 //        getNode();
 //        getChildren();
@@ -43,7 +45,7 @@ public class ZkClientApi {
     }
 
     private static void updateData(){
-        zkClient.writeData("/node_zkClinet_1", new User(2,"yangdehong"));
+        zkClient.writeData("/node_zkClient_1", new User(2,"yangdehong"));
     }
 
     private static void getChildren(){
@@ -53,17 +55,20 @@ public class ZkClientApi {
 
     private static void getNode(){
         Stat stat = new Stat();
-        User user = zkClient.readData("/node_zkClinet_1", stat);
+        User user = zkClient.readData("/node_zkClient_1", stat);
         System.out.println(user);
     }
 
     private static void deleteNode(){
         // 其他的creatxxx都是类似的
-        zkClient.delete("/node_zkClinet_1");
+        zkClient.delete("/node_zkClient_1");
+        // 递归删除，删除指定节点和所有子节点
+//        zkClient.deleteRecursive()
     }
 
     private static void createNode(){
-        // 其他的creatxxx都是类似的
-        zkClient.createPersistentSequential("/node_zkClinet_1/children", new User(1,"ydh"));
+        // 其他的creatxxx都是类似的，true是递归创建，如果要递归删除只能这样
+        zkClient.createPersistent("/node_zkClient_1/children2", true);
+//        zkClient.createPersistent("/node_zkClient_2", new User(1,"ydh"));
     }
 }
